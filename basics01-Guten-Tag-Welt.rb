b6b5a0d30b9f1a4d033b6cbe6bd2f853561cde60
Mike Wei <53706377+seth-wm@users.noreply.github.com>
2019-09-20 09:43:39 +0200

    Create basics01-Guten-Tag-Welt.rb

---
+++ basics01-Guten-Tag-Welt.rb
@@ -0,0 +1,28 @@
+#--Nejznamejsi progam v galaxii - HelloWorld--
+puts "Guten Tag, Welt"
+#prikaz puts - PUT String - vypise textovy retezec na obrazovku (se zalomenim radku)
+#puts [string]
+
+#print -  vypiste text/promennou na obrazovku
+number = 20
+print "Je mi ",number," let. Jsem extra mlady"
+#jednotlive objekty je potreba oddelit carkou, v nasem pripade [string],[integer],[string]
+#nezalamuje radek automaticky - escape char \n!
+print "Ahoj"
+print "CocaCola"
+#prikazy vypisi AhojCocaCola
+print "Ahoj\n"
+print "CocaCola"
+#prikazy vypisi:
+#Ahoj
+#CocaCola
+
+#--Operatory--
+#UNARNI
+#! - negace
+puts (!true) #vypise "false" na obrazovku
+#~ - bitovy doplnek
+puts (~4) #vypise "-5"
+#+ - unarni plus
+
+#ARITMETICKE
